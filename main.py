4248c716e73e1613d25676ed4683f8160331dc04
Anurag Bhattacharjee <105806479+AnuragB2004@users.noreply.github.com>
2024-10-05 18:51:55 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import os
+
+import PyPDF2 as pdf
+import google.generativeai as genai
+import streamlit as st
+from dotenv import load_dotenv
+from streamlit_extras.add_vertical_space import add_vertical_space
+
+load_dotenv()  # Load environment variables
+
+genai.configure(api_key=os.getenv("AIzaSyBBmWRncgbLw7a_Ur1YTts4OSgo_Zg6I4Y"))
+
+
+def get_gemini_response(input):
+    model = genai.GenerativeModel('gemini-pro')
+    response = model.generate_content(input)
+    return response.text
+
+
+def input_pdf_text(uploaded_file):
+    reader = pdf.PdfReader(uploaded_file)
+    text = ""
+    for page in range(len(reader.pages)):
+        page = reader.pages[page]
+        text += str(page.extract_text())
+    return text
+
+
+# Prompt Template
+input_prompt = """
+Hey Act Like a skilled or very experienced ATS(Application Tracking System)
+with a deep understanding of tech field, software engineering, data science, data analyst
+and big data engineering. Your task is to evaluate the resume based on the given job description.
+You must consider the job market is very competitive and you should provide 
+best assistance for improving the resumes. Assign the percentage matching based 
+on JD and
+the missing keywords with high accuracy.
+resume: {text}
+description: {jd}
+
+I want the response as per below structure:
+{{"JD Match": "%", "MissingKeywords": [], "Profile Summary": ""}}
+"""
+
+# Streamlit app
+with st.sidebar:
+    st.title("Smart ATS for Resumes")
+    st.subheader("About")
+    st.write(
+        "This sophisticated ATS project, developed with Gemini Pro and Streamlit, seamlessly incorporates advanced features including resume match percentage, keyword analysis to identify missing criteria, and the generation of comprehensive profile summaries, enhancing the efficiency and precision of the candidate evaluation process for discerning talent acquisition professionals.")
+
+    st.markdown("""
+    - [Streamlit](https://streamlit.io/)
+    - [Gemini Pro](https://deepmind.google/technologies/gemini/#introduction)
+    - [makersuite API Key](https://makersuite.google.com/)
+    - [Github](https://github.com/praj2408/End-To-End-Resume-ATS-Tracking-LLM-Project-With-Google-Gemini-Pro) Repository
+    """)
+
+
+st.title("Smart Application Tracking System")
+st.text("Improve Your Resume ATS")
+jd = st.text_area("Paste the Job Description")
+uploaded_file = st.file_uploader("Upload Your Resume", type="pdf", help="Please upload the pdf")
+
+submit = st.button("Submit")
+
+if submit:
+    if uploaded_file is not None:
+        text = input_pdf_text(uploaded_file)
+        # Generate response using both resume text and job description
+        response = get_gemini_response(input_prompt.format(text=text, jd=jd))
+        st.subheader(response)
+    else:
+        st.error("Please upload a valid PDF resume.")
